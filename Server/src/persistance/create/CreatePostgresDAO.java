2dc37019cbf23e251065eedbe76839cf1ab9fcc7
Carolina Valencia Zavidei <zavidei.carolina@gmail.com>
2025-05-03 03:08:45 +0200

    did the database yay

--- Server/src/persistance/create/CreatePostgresDAO.java
+++ Server/src/persistance/create/CreatePostgresDAO.java
@@ -4,7 +4,9 @@ import model.entities.Activity;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreatePostgresDAO implements CreateDAO
 {
@@ -27,28 +29,96 @@ public class CreatePostgresDAO implements CreateDAO
   private static Connection getConnection() throws SQLException
   {
     return DriverManager.getConnection(
-        "jdbc:postgresql://localhost:5432/viahack?currentSchema=jdbc",
-        "postgres", "1412");
+        "jdbc:postgresql://localhost:5432/postgres?currentSchema=viahack",
+        "postgres", "password");
   }
 
+  private static final Map<String, Integer> ACTIVITY_TYPES = new HashMap<>();
+  static
+  {
+    ACTIVITY_TYPES.put("Music", 1);
+    ACTIVITY_TYPES.put("Sports", 2);
+    ACTIVITY_TYPES.put("Technology", 3);
+    ACTIVITY_TYPES.put("Art", 4);
+    ACTIVITY_TYPES.put("Literature", 5);
+    ACTIVITY_TYPES.put("Gaming", 6);
+    ACTIVITY_TYPES.put("Cooking", 7);
+    ACTIVITY_TYPES.put("Fitness", 8);
+    ACTIVITY_TYPES.put("Film", 9);
+    ACTIVITY_TYPES.put("Nature", 10);
+
+  }
   @Override public Activity create(String name, String type, String address,
       String city, String meetingDate, String meetingTime, int price,
       String description)
   {
     try (Connection connection = getConnection())
     {
+
+      // Debug print statements
+      System.out.println("Inserting activity:");
+      System.out.println("Name: " + name);
+      System.out.println("Type: " + type);
+      System.out.println("Address: " + address);
+      System.out.println("City: " + city);
+      System.out.println("Date: " + meetingDate);
+      System.out.println("Time: " + meetingTime);
+      System.out.println("Price: " + price);
+      System.out.println("Description: " + description);
+
+      int typeId;
+      switch(type)
+      {
+        case "Music":
+          typeId = 1;
+          break;
+        case "Sports":
+          typeId = 2;
+          break;
+        case "Technology":
+          typeId = 3;
+          break;
+        case "Art":
+          typeId = 4;
+          break;
+        case "Literature":
+          typeId = 5;
+          break;
+        case "Gaming":
+          typeId = 6;
+          break;
+        case "Cooking":
+          typeId = 7;
+          break;
+        case "Fitness":
+          typeId = 8;
+          break;
+        case "Film":
+          typeId = 9;
+          break;
+        case "Nature":
+          typeId = 10;
+          break;
+        default:
+          throw new IllegalArgumentException("Invalid activity type: " + type);
+      }
+
       PreparedStatement statement = connection.prepareStatement(
           "INSERT INTO activity(name, type, address, city, meeting_date, meeting_time, price, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?);",
           PreparedStatement.RETURN_GENERATED_KEYS);
+
       statement.setString(1, name);
-      statement.setString(2, type);
+      statement.setInt(2, typeId);
       statement.setString(3, address);
       statement.setString(4, city);
-      statement.setString(6, meetingDate);
-      statement.setString(7, meetingTime);
-      statement.setInt(5, price);
+      statement.setString(5, meetingDate);
+      statement.setString(6, meetingTime);
+      statement.setInt(7, price);
       statement.setString(8, description);
-      statement.executeUpdate();
+
+      int rowsAffected = statement.executeUpdate();
+      System.out.println("Rows inserted: " + rowsAffected);
+
       ResultSet keys = statement.getGeneratedKeys();
       if (keys.next())
       {
@@ -62,6 +132,7 @@ public class CreatePostgresDAO implements CreateDAO
     }
     catch (SQLException e)
     {
+      System.err.println("SQL Insert Error: " + e.getMessage());
       e.printStackTrace();
     }
     return null;
